6bcf95fc8e25329f775ef17ec228969cb4da248b
furdikno <tetetey11235@gmail.com>
2021-04-19 16:30:13 +0300

    initialized connection to an actual db

--- teamdev/settings.py
+++ teamdev/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'teamdev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_pycopg2',
+        'NAME': 'teamdev',
+        'USER': 'postgres',
+        'PASSWORD': 'music3vat',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 

b65fe5066c1718a476857f36bc15a86694fd5436
furdikno <tetetey11235@gmail.com>
2021-04-22 22:44:28 +0300

    TDP-43 added pagination

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
+
+
+def paginate(objects_list, request, per_page=5):
+    limit = request.GET.get('limit', per_page)
+    paginator = Paginator(objects_list, limit)
+    page = request.GET.get('page')
+    objects_page_list = paginator.get_page(page)
+    return objects_page_list

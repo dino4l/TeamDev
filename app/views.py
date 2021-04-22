9a3aa40511c8d9ed40a3efd1ad7a0bb98c8fde84
furdikno <tetetey11235@gmail.com>
2021-04-22 22:46:53 +0300

    TDP-42 added a func to get new questions

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
+from app.models import Profile, Question
 
 def paginate(objects_list, request, per_page=5):
     limit = request.GET.get('limit', per_page)
@@ -10,3 +11,15 @@ def paginate(objects_list, request, per_page=5):
     page = request.GET.get('page')
     objects_page_list = paginator.get_page(page)
     return objects_page_list
+
+
+def new_questions(request):
+
+    db_question = Question.objects.newest()
+    pag_questions = paginate(db_question, request)
+
+    return render(request, 'new_questions.html', {
+        'questions': pag_questions,
+        'tags': Tag.objects.popular(),
+        'best_members': Profile.objects.best()
+    })

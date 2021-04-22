ddf7d62584fe71021f7411a76fec5fe5e4280a56
Tsoraev Rolan <rolicsson@mail.ru>
2021-04-22 23:16:32 +0300

    TDP-46 added profile and tag serializer

--- app/serializers.py
+++ app/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
-from .models import Question, Comment
+from .models import Profile, Tag, Question, Comment
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('id', 'name', 'image', 'user')
+        extra_kwargs = {
+            'name': {'write_only': True}
+        }
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = ['id', 'title', 'text', 'data_create', 'rating', 'author', 'tags']
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'title']
+        
+        
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment

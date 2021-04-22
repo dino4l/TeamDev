259dcc82b808ebe2917e112a19f94d089f88a45e
Tsoraev Rolan <rolicsson@mail.ru>
2021-04-22 23:00:27 +0300

    TDP-42 added a func to get hot questions

--- app/views.py
+++ app/views.py
@@ -23,3 +23,14 @@ def new_questions(request):
         'tags': Tag.objects.popular(),
         'best_members': Profile.objects.best()
     })
+    
+def hot_questions(request):
+
+    db_question = Question.objects.hottest()
+    pag_questions = paginate(db_question, request)
+
+    return render(request, 'hot_questions.html', {
+        'questions': pag_questions,
+        'tags': Tag.objects.popular(),
+        'best_members': Profile.objects.best()
+    })

1e8d4997d8b27fc59e89c51d1cb7498219dd03f9
furdikno <tetetey11235@gmail.com>
2021-04-22 23:28:48 +0300

    TDP-48 added an avatar form

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,20 @@
 from django import forms
 
+from django.contrib.auth.models import User
+
 class LoginForm(forms.Form):
     username = forms.CharField(required=True,)
     password = forms.CharField(required=True, widget=forms.PasswordInput())
+
+class AvatarForm(forms.ModelForm):
+    avatar = forms.ImageField()
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'avatar']
+
+    def save(self, *args, **kwargs):
+        user = super().save(*args, **kwargs)
+        user.profile.image = self.cleaned_data['avatar']
+        user.profile.save()
+        return user

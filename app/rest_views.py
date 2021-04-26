dd8c783d5d06376e75a7c43770d755572b2dc04b
furdikno <tetetey11235@gmail.com>
2021-04-26 16:34:11 +0300

    TDP-37 added an authentication rest-view

--- app/rest_views.py
+++ app/rest_views.py
@@ -104,3 +104,44 @@ def question_tag_view(request, slug):
     questions = questions_by_tag_case(slug, page, limit)
     serializer = QuestionSerializer(questions, many=True)
     return Response(serializer.data)
+
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def signup_view(request):
+    if request.method == 'GET':
+        user = create_user_case()
+        serializer = ProfileSerializer(user)
+        st = status.HTTP_200_OK
+    elif request.method == 'POST':
+        user = create_user_case(request)
+        serializer = ProfileSerializer(user)
+        if serializer.is_valid():
+            st = status.HTTP_201_CREATED
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response('Error', status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.data, status=st)
+
+@api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
+def login_view(request):
+    if request.method == 'GET':
+        user = login_user_case()
+        serializer = ProfileSerializer(user, many=True)
+        st = status.HTTP_200_OK
+    elif request.method == 'POST':
+        user = login_user_case(request, request.POST)
+        serializer = ProfileSerializer(data=request.POST, many=True)
+        if serializer.is_valid():
+            st = status.HTTP_200_OK
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response('Error', status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.data, status=st)
+
+@api_view(['GET'])
+def logout_view(request):
+    logout_user_case(request)
+    return Response('Logout', status=status.HTTP_202_ACCEPTED)

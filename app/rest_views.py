16742c8bcac89fccf0d11cfe73f9f426df20ad42
furdikno <tetetey11235@gmail.com>
2021-04-26 16:29:59 +0300

    TDP-37 added a rest-index view

---
+++ app/rest_views.py
@@ -0,0 +1,25 @@
+from rest_framework import status, decorators, response, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+from .use_cases import *
+from .serializers import *
+
+@api_view(['GET', 'POST'])
+def index_view(request):
+    if request.method == 'GET':
+        page = request.GET.get('page', 1)
+        limit = request.GET.get('limit', 10)
+        questions = new_questions_case(page, limit)
+        serializer = QuestionSerializer(questions, many=True)
+        st = status.HTTP_200_OK
+    elif request.method == 'POST':
+        question = create_question_case(request.POST, request.user)
+        serializer = QuestionSerializer(question)
+        if serializer.is_valid():
+            st = status.HTTP_201_CREATED
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response('Error', status=status.HTTP_400_BAD_REQUEST)
+    return Response(serializer.data, status=st)
